2feae63f84ca857e5fd9904cf85dd975e9e515db
Math2okt <matias.marecos@fiuni.edu.py>
2026-09-09 20:18:49 -0300

    feat: first spring config

---
+++ src/main/java/com/example/appevecommon/AppEveCommonApplication.java
@@ -0,0 +1,14 @@
+package com.example.appevecommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppEveCommonApplication {
+
+    public static void main(String[] args) {
+        System.out.println("HOLA MUNDOOOOOOO");
+        SpringApplication.run(AppEveCommonApplication.class, args);
+    }
+
+}
