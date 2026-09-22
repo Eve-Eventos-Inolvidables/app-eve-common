ad945be0390dbcd9a34f509c6b538d3867105598
Math2okt <matias.marecos@fiuni.edu.py>
2026-09-21 22:25:37 -0300

    feat: IBaseService implementation

--- src/main/java/com/example/appevecommon/Service/IBaseService.java
+++ src/main/java/com/example/appevecommon/Service/IBaseService.java
@@ -1,11 +1,12 @@
 package com.example.appevecommon.Service;
 
 import com.example.appevecommon.Models.Base.BaseEntity;
+import com.example.appevecommon.Service.Utilities.PagedFilter;
 import com.example.appevecommon.Service.Utilities.Responses.PageResult;
 
 import java.util.List;
 
-public interface IBaseService <X extends BaseEntity, D, F > { //D = DTO F = Filter
+public interface IBaseService<E extends BaseEntity, D, F extends PagedFilter> { //D = DTO F = Filter
     //CRUD
     D create(D dto);
     D getById(Long id);
@@ -17,8 +18,8 @@ public interface IBaseService <X extends BaseEntity, D, F > { //D = DTO F = Filt
         throw new UnsupportedOperationException("getAll() no implementado");
     }
 
-    //Paginated with filter(optional)
-    default PageResult<D> getByFilter(Integer page, F filter) {
+    //paginado con filtro opcional (filter == null → trae todo paginado)
+    default PageResult<D> getByFilter(F filter) {
         throw new UnsupportedOperationException("getByFilter() no implementado");
     }
 
